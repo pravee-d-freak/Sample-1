fdcf530384f2c3696a4785e916c415a61d0a7c4f
SanjuChinna <tjagadeesh37@gmail.com>
2017-02-19 02:18:52 -0800

    initial commit

---
+++ Hai.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Hai{
+public static void main(String args[]){
+int i;
+Scanner input=new Scanner(System.in);
+System.out.println("enter any no:");
+int s=input.nextInt();
+if(s<10){
+System.out.prinltn("Hello");
+
+}
+
+}
+
+}
